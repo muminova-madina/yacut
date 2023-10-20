769cecafb8c358852f25e1abf9aae344478c7876
madina <madi92@mail.ru>
2023-10-20 15:19:18 +0300

    second commit

--- yacut/models.py
+++ yacut/models.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from yacut import db
 from yacut import constants as const
-from yacut.id_validation import generate_short_id, is_free_short_id, get_unique_short_id
 
 
 class URLMap(db.Model):
@@ -11,15 +10,3 @@ class URLMap(db.Model):
     original = db.Column(db.String, nullable=False)
     short = db.Column(db.String(const.CUSTOM_ID_LENGTH), nullable=False, unique=True, index=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-
-    @staticmethod
-    def generate_short_id(length):
-        return generate_short_id(length)
-
-    @staticmethod
-    def is_free_short_id(short_id):
-        return is_free_short_id(short_id)
-
-    @staticmethod
-    def get_unique_short_id(length):
-        return get_unique_short_id(length)
--- yacut/views.py
+++ yacut/views.py
@@ -1,3 +1,6 @@
+import string
+from random import choice
+
 from flask import Response, redirect, render_template, url_for
 
 from yacut import app
@@ -47,3 +50,20 @@ def mapping_redirect(short_id: str) -> Response:
     return redirect(
         URLMap.query.filter_by(short=short_id).first_or_404().original,
     )
+
+
+def generate_short_id(length):
+    symbols = string.ascii_letters + string.digits
+    return "".join(choice(symbols) for _ in range(length))
+
+
+def is_free_short_id(short_id):
+    return URLMap.query.filter_by(short=short_id).first() is None
+
+
+def get_unique_short_id(length):
+    while True:
+        short_id = generate_short_id(length)
+        if is_free_short_id(short_id):
+            break
+    return short_id
